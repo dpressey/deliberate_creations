7d1b20e90e5043b77f5109227386ccb734715731
Denzel Pressey <pressey90@gmail.com>
2013-11-02 18:15:10 -0400

    added jquery to the portfolio page, changed brand font

---
+++ photos.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$("#button-2006").on('click', function(){
+		$(".photos-2006").find("li").slideToggle();	
+	});
+	$("#button-2007").on('click', function(){
+		$(".photos-2007").find("li").slideToggle();	
+	});
+	$("#button-2008").on('click', function(){
+		$(".photos-2008").find("li").slideToggle();	
+	});
+	$("#button-2009").on('click', function(){
+		$(".photos-2009").find("li").slideToggle();	
+	});
+});
